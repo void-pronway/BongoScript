d7f69b4fe16927439817dbae00bf5eeb8240bd5d
void-pronway <pronwaymitra135@gmail.com>
2026-08-11 04:00:16 +0600

    Return proper compiler exit codes

--- src/main/main.c
+++ src/main/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include "../parser/semantic.h"
 
-
 int yyparse(void);
 
 int main(void)
 {
+    int parse_result;
+
     printf("BongoScript Compiler\n");
     printf("--------------------\n");
 
     semantic_init();
-    yyparse();
+
+    parse_result = yyparse();
+
+    if (parse_result != 0) {
+        return 1;
+    }
 
     return 0;
 }
